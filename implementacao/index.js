2c913292d59528ae4013cc653b02ef4643a18174
Amanda Cruz Lima <amandacruzlima.ac@gmail.com>
2025-06-24 17:44:59 -0300

    adicionando js nos nav-links

---
+++ implementacao/index.js
@@ -0,0 +1,16 @@
+// hover e focus da navbar
+function handleFirstTab(e) {
+  if (e.key === "Tab") {
+    document.body.classList.add("using-keyboard");
+    window.removeEventListener("keydown", handleFirstTab);
+    window.addEventListener("mousedown", handleMouseDownOnce);
+  }
+}
+
+function handleMouseDownOnce() {
+  document.body.classList.remove("using-keyboard");
+  window.removeEventListener("mousedown", handleMouseDownOnce);
+  window.addEventListener("keydown", handleFirstTab);
+}
+
+window.addEventListener("keydown", handleFirstTab);
